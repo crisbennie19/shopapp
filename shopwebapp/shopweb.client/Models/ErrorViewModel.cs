d8ecbb51a7badc1e10dd02a278d00ccc9240e94f
crisbennie19 <akowechristian@gmail.com>
2023-12-24 05:58:37 +0100

    Secomd update

---
+++ shopwebapp/shopweb.client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shopweb.client.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
